09ff1205502bfcb7040f3df333e7e2196e12d5d1
LauraTrehout <lauratrehout@gmail.com>
2022-10-27 10:07:27 +0200

    add test

--- code/07-finished/src/data.test.js
+++ code/07-finished/src/data.test.js
@@ -1,9 +1,10 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi } from "vitest";
 
-import { generateReportData } from './data';
+import { generateReportData, storeData } from "./data";
 
-describe('generateReportData()', () => {
-  it('should execute logFn if provided', () => {
+
+describe("generateReportData()", () => {
+  it("should execute logFn if provided", () => {
     const logger = vi.fn();
 
     // logger.mockImplementationOnce(() => {});
@@ -11,5 +12,26 @@ describe('generateReportData()', () => {
     generateReportData(logger);
 
     expect(logger).toBeCalled();
+  });
+});
+
+describe("storeData()", () => {
+  it("should throw an error if no data is provided", () => {
+    const data = undefined;
+    const result = () => storeData(data);
+    return expect(result).rejects.toThrow(/no data received/i);
+  });
+  it("should call writeData with correct arguments", async () => {
+   const data = 'test'
+    const writeData = vi.fn();
+    const storeData = vi.fn((data) => {
+      return new Promise((resolve, reject) => {
+        resolve(writeData(data, "test.txt"));
+      });
+    });
+    const result = storeData(data);
+    expect(writeData).toHaveBeenCalledTimes(1);
+    expect(writeData).toHaveBeenCalledWith("test", "test.txt");
+    return expect(result).resolves.toBe();
   });
 });
