312203a360ed586d58877371a9edd3742401bb96
LauraTrehout <lauratrehout@gmail.com>
2022-10-27 10:26:30 +0200

    update test

--- code/07-finished/src/data.test.js
+++ code/07-finished/src/data.test.js
@@ -2,6 +2,12 @@ import { describe, it, expect, vi } from "vitest";
 
 import { generateReportData, storeData } from "./data";
 
+const writeData = vi.fn(() => 'ok');
+const mockStoreData = vi.fn((data) => {
+  return new Promise((resolve, reject) => {
+    resolve(writeData(data, "test.txt"));
+  });
+});
 
 describe("generateReportData()", () => {
   it("should execute logFn if provided", () => {
@@ -21,17 +27,16 @@ describe("storeData()", () => {
     const result = () => storeData(data);
     return expect(result).rejects.toThrow(/no data received/i);
   });
-  it("should call writeData with correct arguments", async () => {
-   const data = 'test'
-    const writeData = vi.fn();
-    const storeData = vi.fn((data) => {
-      return new Promise((resolve, reject) => {
-        resolve(writeData(data, "test.txt"));
-      });
-    });
-    const result = storeData(data);
+  it("should call writeData with correct arguments", () => {
+    const data = "test";
+
+    mockStoreData(data);
     expect(writeData).toHaveBeenCalledTimes(1);
     expect(writeData).toHaveBeenCalledWith("test", "test.txt");
-    return expect(result).resolves.toBe();
+  });
+  it("should resolve to no value if called correctly", async () => {
+    const data = "test";
+    const result = await mockStoreData(data);
+    expect(result).toBe('ok')
   });
 });
